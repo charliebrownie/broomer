780047ee858f9fd84339b19b0a99f96db9a287f5
Mai Morita <mai.nishio0315@gmail.com>
2019-07-23 20:53:53 +0900

    updated edit/destroy/mypage/comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_093151) do
+ActiveRecord::Schema.define(version: 2019_07_23_083328) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.text "text"
     t.text "image1"
     t.text "image2"
