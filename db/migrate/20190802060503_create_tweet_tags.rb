18c6065dc8ceee1fa2e94d362ddf477e0c50c166
Mai Morita <mai.nishio0315@gmail.com>
2019-08-02 15:18:54 +0900

    updated

--- db/migrate/20190802060503_create_tweet_tags.rb
+++ db/migrate/20190802060503_create_tweet_tags.rb
@@ -1,8 +1,8 @@
 class CreateTweetTags < ActiveRecord::Migration[5.2]
   def change
     create_table :tweet_tags do |t|
-      t.references :tweet, index: true, foreign_key: true
-      t.references :tag, index: true, foreign_key: true
+      t.references :tweet, foreign_key: true
+      t.references :tag, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_27_101318) do
+ActiveRecord::Schema.define(version: 2019_08_02_060503) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
