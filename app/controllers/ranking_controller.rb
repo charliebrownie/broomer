39ba66314bf2f9ba21dd42fc5aa989737e2188c7
Mai Morita <mai.nishio0315@gmail.com>
2019-08-02 16:48:56 +0900

    updated ranking

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,7 +1,9 @@
 class RankingController < ApplicationController
   def index
     # @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
-    @all_ranks = Tweet.order("likes_count DESC").limit(6)
+    @no_null = Tweet.where("likes_count is not null").order("likes_count desc").limit(6)
+    @yes_null = Tweet.where("likes_count is null").limit(6 - @no_null.count)
+    @all_ranks = @no_null + @yes_null
     # @all_ranks = Tweet.find(Tweet.group(:id).order('count(likes_count) desc').limit(3).pluck(:id))
     # all_ranks = Tweet.group(:id).order('count_likes_count desc').limit(3).count(:likes_count).keys
     # @all_ranks = all_ranks.map{|id| Tweet.find(id)}
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,7 @@ class TweetsController < ApplicationController
 
   def index
     @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
-    @all_ranks = Tweet.order("likes_count DESC").limit(3)
+    # @all_ranks = Tweet.order("likes_count DESC").limit(6)
     # @all_ranks = Tweet.find(Tweet.group(:id).order('count(likes_count) desc').limit(3).pluck(:id))
     # all_ranks = Tweet.group(:id).order('count_likes_count desc').limit(3).count(:likes_count).keys
     # @all_ranks = all_ranks.map{|id| Tweet.find(id)}
@@ -44,7 +44,7 @@ class TweetsController < ApplicationController
 
   private
   def create_params
-    params.require(:tweet).permit(:text, :image1, :image2, :tag_ids).merge(user_id: current_user.id)
+    params.require(:tweet).permit(:text, :image1, :image2, :tag_ids, :likes_count).merge(user_id: current_user.id)
   end
 
   def move_to_index
